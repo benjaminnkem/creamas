388c151ba7307a5b757a4feb1424041cf4c7bde5
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-10-30 12:42:41 +0100

    update: added new ui to section

--- components/UI/Home/sticky-sect.tsx
+++ components/UI/Home/sticky-sect.tsx
@@ -2,7 +2,7 @@ import WidthClamp from "@/components/Common/Custom/width-clamp";
 import classNames from "classnames";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { User2Icon } from "lucide-react";
+import { ArrowLeft, ArrowLeftIcon, ArrowRight, ExternalLink, Search, User2Icon } from "lucide-react";
 import { useLayoutEffect, useRef } from "react";
 
 const HomeStickySect = () => {
@@ -41,6 +41,8 @@ const HomeStickySect = () => {
   useLayoutEffect(() => {
     const cxt = gsap.context(() => {
       const t1 = gsap.timeline();
+
+      const pinSections = gsap.utils.toArray(".pin-sec");
 
       t1.from(".pin-sect-2", { xPercent: 100, ease: "none", duration: 2 });
       t1.from(".pin-sect-3", { xPercent: 100, ease: "none", duration: 2 });
@@ -110,7 +112,91 @@ const HomeStickySect = () => {
       </section>
 
       <div ref={pinSection} className="relative h-screen">
-        {new Array(4).fill(null).map((_, idx) => {
+        <section className={`w-full absolute top-0 left-0 h-full flex items-center bg-[#ae7769] pin-sec pin-sect-1`}>
+          <WidthClamp>
+            <div className="space-y-2">
+              <h4 className="text-[8rem] font-extrabold">We Buy</h4>
+              <div className="space-y-6">
+                <p className="text-2xl font-light max-w-3xl">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, atque. Lorem, ipsum dolor sit amet
+                  consectetur adipisicing elit. Beatae vero dicta, delectus excepturi et accusantium modi! Molestiae
+                  mollitia
+                </p>
+
+                <button className="text-xl flex items-center gap-2 group border border-[#e6b7abd6] px-4 duration-300 hover:px-5 py-2">
+                  <span>Contact Us</span>
+                  <ArrowRight className="group-hover:translate-x-4 transition-transform duration-300" />
+                </button>
+              </div>
+            </div>
+          </WidthClamp>
+        </section>
+
+        <section className={`w-full absolute top-0 left-0 h-full flex items-center bg-[#9c6b5f] pin-sec pin-sect-2`}>
+          <WidthClamp>
+            <div className="space-y-2 text-right">
+              <h4 className="text-[8rem] font-extrabold">We Sell</h4>
+              <div className="space-y-6">
+                <p className="text-2xl font-light max-w-3xl ml-auto">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, atque. Lorem, ipsum dolor sit amet
+                  consectetur adipisicing elit. Beatae vero dicta, delectus excepturi et accusantium modi! Molestiae
+                  mollitia
+                </p>
+
+                <button className="text-xl float-right flex items-center gap-2 group border border-[#e6b7abd6] px-4 duration-300 hover:px-5 py-2">
+                  <ArrowLeft className="group-hover:-translate-x-4 transition-transform duration-300" />
+                  <span>Contact Us</span>
+                </button>
+              </div>
+            </div>
+          </WidthClamp>
+        </section>
+
+        <section className={`w-full absolute top-0 left-0 h-full flex items-center bg-[#ae7769] pin-sec pin-sect-3`}>
+          <WidthClamp>
+            <div className="space-y-2">
+              <h4 className="text-[8rem] font-extrabold">Visit Our Showcase</h4>
+              <div className="space-y-6">
+                <p className="text-2xl font-light max-w-3xl">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, atque. Lorem, ipsum dolor sit amet
+                  consectetur adipisicing elit. Beatae vero dicta, delectus! Molestiae mollitia
+                </p>
+
+                <button className="text-xl flex items-center gap-2 group border border-[#e6b7abd6] px-4 duration-300 py-2">
+                  <span>Visit Showcase</span>
+
+                  <ExternalLink className="transition-transform duration-300" />
+                </button>
+              </div>
+            </div>
+          </WidthClamp>
+        </section>
+
+        <section
+          className={`w-full absolute top-0 left-0 h-full flex text-center items-center bg-[#9a695d] pin-sec pin-sect-4`}
+        >
+          <WidthClamp>
+            <div className="space-y-2">
+              <h4 className="text-[8rem] font-extrabold">Skincare Finder</h4>
+              <div className="space-y-6">
+                <p className="text-2xl font-light max-w-3xl mx-auto">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, atque. Lorem, ipsum dolor sit amet
+                  consectetur adipisicing elit. Beatae vero dicta, delectus! Molestiae mollitia
+                </p>
+
+                <div className="grid place-content-center">
+                  <button className="text-xl flex items-center gap-2 group border border-[#e6b7abd6] px-4 duration-300 py-2">
+                    <span>Find A Skincare Product</span>
+
+                    <Search className="transition-transform duration-300" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </WidthClamp>
+        </section>
+
+        {/* {new Array(4).fill(null).map((_, idx) => {
           const background = classNames({
             "bg-amber-100": idx + 1 === 1,
             "bg-amber-200": idx + 1 === 2,
@@ -120,7 +206,7 @@ const HomeStickySect = () => {
           return (
             <section
               key={idx}
-              className={`${background} w-full absolute top-0 left-0 h-full flex items-center justify-center text-black pin-sect-${
+              className={`${background} w-full absolute top-0 left-0 h-full flex items-center justify-center text-black pin-sec pin-sect-${
                 idx + 1
               }`}
             >
@@ -129,7 +215,7 @@ const HomeStickySect = () => {
               </div>
             </section>
           );
-        })}
+        })} */}
       </div>
 
       <section ref={displayWrap}>
